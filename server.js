24b2dc468059139e990d2c15bc34ba1a082d052b
Marcus Jonas <marcus.jonas14@gmail.com>
2023-12-22 14:48:18 -0600

    updated server with build endpoints

--- server.js
+++ server.js
@@ -23,11 +23,11 @@ app.use(myParser.urlencoded({limit: '200mb', extended: false}));
 app.use(cors());
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
+  app.use(express.static(path.join(__dirname, "./client/build")));
 }
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 const startApolloServer = async () => {
